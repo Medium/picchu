96b6cfc336d70eb2dc0033e797a4fed847a561f8
Lyra Naeseth <lyra@medium.com>
2019-07-03 13:20:58 -0700

    Disable automatic releases during holiday

--- pkg/controller/releasemanager/schedule.go
+++ pkg/controller/releasemanager/schedule.go
@@ -22,8 +22,11 @@ func init() {
 // TODO(lyra): make configurable
 // TODO(lyra): give schedule its own type
 func schedulePermitsRelease(t time.Time, schedule string) bool {
-	if schedule != "humane" && schedule != "Humane" {
-		return true
+	// FIXME(lyra): 2019-07-04 holiday hack
+	// release apps with the "always" schedule as normal
+	// don't release "humane" apps (i.e., lite, rito, tutu) automatically (OOB only)
+	if schedule == "humane" || schedule == "Humane" {
+		return false
 	}
 
 	t = t.In(scheduleLocation).Truncate(time.Hour)
