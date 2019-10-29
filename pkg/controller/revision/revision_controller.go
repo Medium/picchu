afe604482b7000c8345ea38eec366c02293fee58
Lyra Naeseth <lyra@naeseth.com>
2019-10-29 13:51:02 -0700

    Revert "More Aggressive Ramping (#144)" (#146)
    * Revert "Only agro scale release/releasing incarnations (#145)"
    
    This reverts commit ed621521596c57007d84d4a6796e431266aaa5c5.
    
    * Revert "More Aggressive Ramping (#144)"
    
    This reverts commit ca23962780f55375030e9d946ac77d25d2b646d7.

--- pkg/controller/revision/revision_controller.go
+++ pkg/controller/revision/revision_controller.go
@@ -344,6 +344,7 @@ func (r *ReconcileRevision) syncReleaseManager(log logr.Logger, revision *picchu
 		}
 		status.AddReleaseManagerStatus(*rm.RevisionStatus(revision.Spec.App.Tag))
 
+		log.Info("Checking for garbage collection")
 		rmCount++
 		for _, rl := range rm.Status.Revisions {
 			if rl.GitTimestamp == nil {
@@ -364,6 +365,8 @@ func (r *ReconcileRevision) syncReleaseManager(log logr.Logger, revision *picchu
 				} else {
 					log.Info("Expiration not reached", "Expiration", expiration, "GitTimestamp", rl.GitTimestamp, "Ttl", rl.TTL)
 				}
+			} else {
+				log.Info("Tag doesn't match", "StateTag", rl.Tag, "Tag", revision.Spec.App.Tag)
 			}
 		}
 		rstatus.AddTarget(status)
