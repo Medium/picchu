68ee9d5e6ceb2c388d59fca410784b4f32ef52a3
Lyra Naeseth <lyra@medium.com>
2020-02-20 16:56:31 -0800

    Log specific Applier within compositePlan

--- pkg/plan/composite.go
+++ pkg/plan/composite.go
@@ -2,6 +2,7 @@ package plan
 
 import (
 	"context"
+	"reflect"
 
 	"github.com/go-logr/logr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -22,8 +23,10 @@ func All(plans ...Plan) Plan {
 func (p *compositePlan) Apply(ctx context.Context, cli client.Client, scalingFactor float64, log logr.Logger) error {
 	for i := range p.plans {
 		plan := p.plans[i]
+		planType := reflect.TypeOf(plan).Elem()
+
 		log.Info("Applying Plan", "Plan", plan)
-		if err := plan.Apply(ctx, cli, scalingFactor, log); err != nil {
+		if err := plan.Apply(ctx, cli, scalingFactor, log.WithValues("Applier", planType.Name())); err != nil {
 			return err
 		}
 	}
