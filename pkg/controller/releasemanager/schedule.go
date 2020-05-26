0501e3bb04f8bb80c4874104a021c5f3526b481a
Bob Corsaro <bob@medium.com>
2020-05-26 10:23:53 -0400

    Adds holiday schedule to scheduler (#249)

--- pkg/controller/releasemanager/schedule.go
+++ pkg/controller/releasemanager/schedule.go
@@ -7,7 +7,12 @@ import (
 
 var (
 	scheduleLocation *time.Location
+	holidays         []time.Time
 )
+
+func date(year int, month time.Month, day int) time.Time {
+	return time.Date(year, month, day, 0, 0, 0, 0, scheduleLocation)
+}
 
 func init() {
 	// TODO(lyra): make configurable
@@ -17,6 +22,43 @@ func init() {
 	}
 
 	scheduleLocation = loc
+
+	// TODO(bob): Make configurable
+	holidays = []time.Time{
+		date(2020, time.July, 3),      // Day before Independence day
+		date(2020, time.July, 4),      // Saturday, Independence day
+		date(2020, time.July, 5),      // Sunday, Day after Independence day
+		date(2020, time.September, 7), // Labor day
+		date(2020, time.October, 26),  // Day before thanksgiving
+		date(2020, time.October, 27),  // Thanksgiving
+		date(2020, time.December, 22), // Christmas break
+		date(2020, time.December, 23), // Christmas break
+		date(2020, time.December, 24), // Christmas break
+		date(2020, time.December, 25), // Christmas break
+		date(2020, time.December, 26), // Sat, Christmas break
+		date(2020, time.December, 27), // Sunday, Christmas break
+		date(2020, time.December, 28), // Christmas break
+		date(2020, time.December, 29), // Christmas break
+		date(2020, time.December, 30), // Christmas break
+		date(2020, time.December, 31), // Christmas break
+
+		date(2021, time.January, 1),   // New year's day
+		date(2021, time.January, 18),  // MLK day
+		date(2021, time.May, 31),      // Memorial day
+		date(2021, time.July, 4),      // Sunday, Independence day
+		date(2021, time.July, 5),      // Day after Independence day
+		date(2021, time.September, 6), // Labor day
+		date(2021, time.October, 25),  // Thanksgiving
+		date(2021, time.October, 26),  // Day after Thanksgiving
+		date(2021, time.December, 24), // Christmas break
+		date(2021, time.December, 25), // Sat, Christmas break
+		date(2021, time.December, 26), // Sun, Christmas break
+		date(2021, time.December, 27), // Christmas break
+		date(2021, time.December, 28), // Christmas break
+		date(2021, time.December, 29), // Christmas break
+		date(2021, time.December, 30), // Christmas break
+		date(2021, time.December, 31), // Christmas break
+	}
 }
 
 // TODO(lyra): make configurable
@@ -27,6 +69,14 @@ func schedulePermitsRelease(t time.Time, schedule string) bool {
 	}
 
 	t = t.In(scheduleLocation).Truncate(time.Hour)
+
+	day := t.Round(time.Hour * 24)
+	for _, holiday := range holidays {
+		if holiday == day {
+			return false
+		}
+	}
+
 	hour := t.Hour()
 
 	switch t.Weekday() {
