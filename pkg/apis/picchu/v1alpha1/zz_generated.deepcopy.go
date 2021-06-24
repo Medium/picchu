9effdd6ffce9e658767b9750b06a0c988a076ba5
Micah Noland <micah@medium.com>
2021-06-24 10:33:58 -0400

    Support Volume/VolumeMount in Revision

--- pkg/apis/picchu/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/picchu/v1alpha1/zz_generated.deepcopy.go
@@ -1292,6 +1292,20 @@ func (in *RevisionTarget) DeepCopyInto(out *RevisionTarget) {
 			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
+	if in.VolumeMounts != nil {
+		in, out := &in.VolumeMounts, &out.VolumeMounts
+		*out = make([]corev1.VolumeMount, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	if in.Volumes != nil {
+		in, out := &in.Volumes, &out.Volumes
+		*out = make([]corev1.Volume, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
 	if in.PodAnnotations != nil {
 		in, out := &in.PodAnnotations, &out.PodAnnotations
 		*out = make(map[string]string, len(*in))
