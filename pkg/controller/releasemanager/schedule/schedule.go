28153b7bf276a40503a623beb1da48c1e3a90eb8
Julie Russell <julie@medium.com>
2021-12-14 11:58:19 -0800

    updated holiday break to reflect Paris work days

--- pkg/controller/releasemanager/schedule/schedule.go
+++ pkg/controller/releasemanager/schedule/schedule.go
@@ -31,26 +31,6 @@ func init() {
 
 	// TODO(bob): Make configurable
 	holidays = []time.Time{
-		date(2020, time.June, 19),     // Juneteenth
-		date(2020, time.July, 3),      // Day before Independence day
-		date(2020, time.July, 4),      // Saturday, Independence day
-		date(2020, time.July, 5),      // Sunday, Day after Independence day
-		date(2020, time.September, 7), // Labor day
-		date(2020, time.November, 26), // Thanksgiving
-		date(2020, time.November, 27), // Day after thanksgiving
-		date(2020, time.December, 17), // Holiday Freeze
-		date(2020, time.December, 18), // Holiday Freeze
-		date(2020, time.December, 21), // Holiday break
-		date(2020, time.December, 22), // Holiday break
-		date(2020, time.December, 23), // Holiday break
-		date(2020, time.December, 24), // Holiday break
-		date(2020, time.December, 25), // Holiday break
-		date(2020, time.December, 26), // Sat, Holiday break
-		date(2020, time.December, 27), // Sunday, Holiday break
-		date(2020, time.December, 28), // Holiday break
-		date(2020, time.December, 29), // Holiday break
-		date(2020, time.December, 30), // Holiday break
-		date(2020, time.December, 31), // Holiday break
 
 		date(2021, time.January, 1),   // New year's day
 		date(2021, time.January, 18),  // MLK day
@@ -64,10 +44,6 @@ func init() {
 		date(2021, time.December, 24), // Holiday break
 		date(2021, time.December, 25), // Sat, Holiday break
 		date(2021, time.December, 26), // Sun, Holiday break
-		date(2021, time.December, 27), // Holiday break
-		date(2021, time.December, 28), // Holiday break
-		date(2021, time.December, 29), // Holiday break
-		date(2021, time.December, 30), // Holiday break
 		date(2021, time.December, 31), // Holiday break
 	}
 }
