041ce4f3b68b0b6bf35c0fa9b4e40d1642f9e51c
David Osemwengie <dosemwengie@medium.com>
2022-05-10 15:57:31 -0500

    use patch to force protocol default w/path

--- hack/crd-update.sh
+++ hack/crd-update.sh
@@ -4,3 +4,4 @@ go mod tidy
 go mod vendor
 controller-gen +crd:allowDangerousTypes=true,crdVersions=v1beta1 paths=./pkg/... output:crd:dir=./deploy/crds output:stdout
 for crd in $(find ./deploy/crds -type f|grep -v "_crd.yaml$"); do echo $crd; mv $crd ${crd%.yaml}_crd.yaml; done
+git apply ./hack/crd-v1beta1.patch
