5ca59b2b77327f32236ec155e8c0b8868fe89043
David Osemwengie <dosemwengie@medium.com>
2022-05-10 16:04:21 -0500

    debug w/ err

--- hack/crd-update.sh
+++ hack/crd-update.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
 go get sigs.k8s.io/controller-tools/cmd/controller-gen@${1}
 go mod tidy
 go mod vendor
+controller-gen --version
 controller-gen +crd:allowDangerousTypes=true,crdVersions=v1beta1 paths=./pkg/... output:crd:dir=./deploy/crds output:stdout
 for crd in $(find ./deploy/crds -type f|grep -v "_crd.yaml$"); do echo $crd; mv $crd ${crd%.yaml}_crd.yaml; done
 git apply ./hack/crd-v1beta1.patch
