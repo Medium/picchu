00460ea8d11053ea8be1f034748710634cc24e17
Sofie Gonzalez <33813152+sofiegonzalez@users.noreply.github.com>
2022-05-16 09:17:09 -0700

    update to sloth in releasemanager files

--- pkg/controller/releasemanager/plan/syncTaggedServiceLevels.go
+++ pkg/controller/releasemanager/plan/syncTaggedServiceLevels.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"fmt"
 
-	slov1alpha1 "github.com/Medium/service-level-operator/pkg/apis/monitoring/v1alpha1"
+	slov1 "github.com/slok/sloth/pkg/kubernetes/api/sloth/v1"
+
 	"github.com/go-logr/logr"
 	picchuv1alpha1 "go.medium.engineering/picchu/pkg/apis/picchu/v1alpha1"
 	"go.medium.engineering/picchu/pkg/plan"
@@ -39,10 +40,10 @@ func (p *SyncTaggedServiceLevels) Apply(ctx context.Context, cli client.Client, 
 	return nil
 }
 
-func (p *SyncTaggedServiceLevels) serviceLevels(log logr.Logger) (*slov1alpha1.ServiceLevelList, error) {
-	sll := &slov1alpha1.ServiceLevelList{}
-	var sl []slov1alpha1.ServiceLevel
-	var slos []slov1alpha1.SLO
+func (p *SyncTaggedServiceLevels) serviceLevels(log logr.Logger) (*slov1.PrometheusServiceLevelList, error) {
+	sll := &slov1.PrometheusServiceLevelList{}
+	var sl []slov1.PrometheusServiceLevel
+	var slos []slov1.SLO
 
 	for i := range p.ServiceLevelObjectives {
 		if p.ServiceLevelObjectives[i].Enabled {
@@ -54,22 +55,22 @@ func (p *SyncTaggedServiceLevels) serviceLevels(log logr.Logger) (*slov1alpha1.S
 				Labels: p.ServiceLevelObjectiveLabels,
 			}
 			serviceLevelObjective := config.serviceLevelObjective(log)
-			serviceLevelObjective.ServiceLevelIndicator.SLISource.Prometheus = config.taggedSLISource()
+			serviceLevelObjective.SLI.Events = config.taggedSLISource()
 
 			slos = append(slos, *serviceLevelObjective)
 		}
 	}
 
 	if len(slos) > 0 {
-		serviceLevel := &slov1alpha1.ServiceLevel{
+		serviceLevel := &slov1.PrometheusServiceLevel{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      p.taggedServiceLevelName(),
 				Namespace: p.Namespace,
 				Labels:    p.Labels,
 			},
-			Spec: slov1alpha1.ServiceLevelSpec{
-				ServiceLevelName:       p.App,
-				ServiceLevelObjectives: slos,
+			Spec: slov1.PrometheusServiceLevelSpec{
+				Service: p.App,
+				SLOs:    slos,
 			},
 		}
 		sl = append(sl, *serviceLevel)
@@ -79,8 +80,8 @@ func (p *SyncTaggedServiceLevels) serviceLevels(log logr.Logger) (*slov1alpha1.S
 	return sll, nil
 }
 
-func (s *SLOConfig) taggedSLISource() *slov1alpha1.PrometheusSLISource {
-	source := &slov1alpha1.PrometheusSLISource{
+func (s *SLOConfig) taggedSLISource() *slov1.SLIEvents {
+	source := &slov1.SLIEvents{
 		ErrorQuery: s.serviceLevelTaggedErrorQuery(),
 		TotalQuery: s.serviceLevelTaggedTotalQuery(),
 	}
