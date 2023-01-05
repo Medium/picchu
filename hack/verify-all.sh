87d06dcb050b517437b125d96c3162f5ad10694c
Sofie Gonzalez <sgonzalez@medium.com>
2023-01-05 14:17:09 -0800

    git diff in verify

--- hack/verify-all.sh
+++ hack/verify-all.sh
@@ -5,6 +5,7 @@ set -o nounset
 set -o pipefail
 
 GIT_STATUS="$(git status --porcelain)"
+GIT_DIFF="$(git diff)"
 GOFMT_OUT="$(find ./{pkg,cmd}/ -name '*.go' -not -path './go/vendor/*' -not -path './pkg/apis/picchu/v1alpha1/zz_*.go' -exec gofmt -l -s {} \;)"
 
 if [[ -n "${GOFMT_OUT}" || -n "${GIT_STATUS}" ]]; then
@@ -15,6 +16,7 @@ if [[ -n "${GOFMT_OUT}" || -n "${GIT_STATUS}" ]]; then
     if [[ -n "${GIT_STATUS}" ]]; then
       echo "${GIT_STATUS}"
       echo "** git status shows changes"
+      echo "${GIT_DIFF}"
     fi
   exit 1
 fi
