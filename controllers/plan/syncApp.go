1f78c8c2923003aeaaae3b2b1b978718da4ac3d7
Eddie Barth <ebarth@medium.com>
2023-09-22 12:24:17 -0500

    Revert unwanted CRD changes and ONLY update the syncApp.go

--- controllers/plan/syncApp.go
+++ controllers/plan/syncApp.go
@@ -514,10 +514,10 @@ func (p *SyncApp) destinationRule() *istioclient.DestinationRule {
 		newSubset := &istio.Subset{}
 
 		// Check for an empty istio trafficPolicy object, and ONLY add it to the istio subset object
-		// if it's non-empty. Else, do not include it. The subset can be created without it, and we are 
+		// if it's non-empty. Else, do not include it. The subset can be created without it, and we are
 		// seeing a breakage.  In kbfd, the TrafficPolicy is only updated with PortLevelSettings
 		// Ref the go pkg here: https://pkg.go.dev/istio.io/api@v1.19.0/networking/v1alpha3#TrafficPolicy
-		if len(revision.TrafficPolicy.PortLevelSettings) > 0 {
+		if (&istio.TrafficPolicy{}) != revision.TrafficPolicy {
 			newSubset = &istio.Subset{
 				Name:          revision.Tag,
 				Labels:        map[string]string{labelTag: revision.Tag},
@@ -525,8 +525,8 @@ func (p *SyncApp) destinationRule() *istioclient.DestinationRule {
 			}
 		} else {
 			newSubset = &istio.Subset{
-				Name:          revision.Tag,
-				Labels:        map[string]string{labelTag: revision.Tag},
+				Name:   revision.Tag,
+				Labels: map[string]string{labelTag: revision.Tag},
 			}
 		}
 		subsets = append(subsets, newSubset)
