75a96f0b0b3dae10baa57ea9ef027df1b82ced0c
David Osemwengie <thewiz1906@gmail.com>
2023-09-26 12:40:42 -0500

    modify istio json marshalling to camelCase (#394)
    Co-authored-by: David Osemwengie <dosemwengie@medium.com>

--- hack/fix.sh
+++ hack/fix.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 find ./vendor/istio.io -type f -exec grep 'protobuf_oneof' -l {} \; -exec perl -i -pe's/(protobuf_oneof.*)`$/$1 json:\"-\"`/g' {} \;
+find ./vendor/istio.io -type f -exec grep ',omitempty"`$' -l {} \; -exec perl -i -pe 's/(,proto3" json:")([^,"]+)([^"]*)("?,)/$1 . lcfirst(join("", map { ucfirst($_) } split("_", $2))) . $3 . $4/ge;' {} \;
