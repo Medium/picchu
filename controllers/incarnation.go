e975708eea069b1e24693c9a60eb7dc449dfcdb9
Sofie Gonzalez <33813152+sofiegonzalez@users.noreply.github.com>
2023-10-30 12:52:38 -0700

    apply SLO sync only to delivery (#400)

--- controllers/incarnation.go
+++ controllers/incarnation.go
@@ -429,17 +429,14 @@ func (i *Incarnation) deleteCanaryRules(ctx context.Context) error {
 
 func (i *Incarnation) syncTaggedServiceLevels(ctx context.Context) error {
 	if i.picchuConfig.ServiceLevelsFleet != "" && i.picchuConfig.ServiceLevelsNamespace != "" {
-		// Account for a fleet other than Delivery (old way of configuring SLOs) and Production (the only other place we ideally want SLOs to go)
-		err := i.controller.applyPlan(
-			ctx,
-			"Ensure Service Levels Namespace",
-			&rmplan.EnsureNamespace{Name: i.picchuConfig.ServiceLevelsNamespace},
-		)
+		err := i.controller.applyDeliveryPlan(ctx, "Ensure Service Levels Namespace", &rmplan.EnsureNamespace{
+			Name: i.picchuConfig.ServiceLevelsNamespace,
+		})
 		if err != nil {
 			return err
 		}
 
-		return i.controller.applyPlan(ctx, "Sync Tagged Service Levels", &rmplan.SyncTaggedServiceLevels{
+		return i.controller.applyDeliveryPlan(ctx, "Sync Tagged Service Levels", &rmplan.SyncTaggedServiceLevels{
 			App:                         i.appName(),
 			Target:                      i.targetName(),
 			Namespace:                   i.picchuConfig.ServiceLevelsNamespace,
@@ -456,16 +453,12 @@ func (i *Incarnation) syncTaggedServiceLevels(ctx context.Context) error {
 
 func (i *Incarnation) deleteTaggedServiceLevels(ctx context.Context) error {
 	if i.picchuConfig.ServiceLevelsFleet != "" && i.picchuConfig.ServiceLevelsNamespace != "" {
-		return i.controller.applyPlan(
-			ctx,
-			"Delete Tagged Service Levels",
-			&rmplan.DeleteTaggedServiceLevels{
-				App:       i.appName(),
-				Target:    i.targetName(),
-				Namespace: i.picchuConfig.ServiceLevelsNamespace,
-				Tag:       i.tag,
-			},
-		)
+		return i.controller.applyDeliveryPlan(ctx, "Delete Tagged Service Levels", &rmplan.DeleteTaggedServiceLevels{
+			App:       i.appName(),
+			Target:    i.targetName(),
+			Namespace: i.picchuConfig.ServiceLevelsNamespace,
+			Tag:       i.tag,
+		})
 	}
 	i.log.Info("service-levels-fleet and service-levels-namespace not set, skipping DeleteTaggedServiceLevels")
 	return nil
