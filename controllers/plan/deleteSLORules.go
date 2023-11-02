fbb40e30ec1f7b9dcc9a0da6a64db88bc3380a6c
Eddie Barth <ebarth@medium.com>
2023-11-02 12:00:32 -0500

    Update the Picchu labels.SelectFromSet because it excludes PrometheusRules

--- controllers/plan/deleteSLORules.go
+++ controllers/plan/deleteSLORules.go
@@ -24,7 +24,6 @@ func (p *DeleteSLORules) Apply(ctx context.Context, cli client.Client, cluster *
 		Namespace: p.Namespace,
 		LabelSelector: labels.SelectorFromSet(map[string]string{
 			picchuv1alpha1.LabelApp:      p.App,
-			picchuv1alpha1.LabelRuleType: RuleTypeSLO,
 		}),
 	}
 
