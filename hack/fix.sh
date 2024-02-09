853c4ff497d53e65341f4d3ba3ee5f911d15b135
David Osemwengie <thewiz1906@gmail.com>
2024-02-09 13:49:03 -0600

    Revert "fix casing for h2Upgrade Istio settings (#419)" (#421)
    This reverts commit eeb9e6fb5a0c381783d185340763f0721fb7dd88.

--- hack/fix.sh
+++ hack/fix.sh
@@ -2,4 +2,3 @@
 
 find ./vendor/istio.io -type f -exec grep 'protobuf_oneof' -l {} \; -exec perl -i -pe's/(protobuf_oneof.*)`$/$1 json:\"-\"`/g' {} \;
 find ./vendor/istio.io -type f -exec grep ',omitempty"`$' -l {} \; -exec perl -i -pe 's/(,proto3" json:")([^,"]+)([^"]*)("?,)/$1 . lcfirst(join("", map { ucfirst($_) } split("_", $2))) . $3 . $4/ge;' {} \;
-find ./vendor/istio.io -type f -exec grep 'h2_upgrade_policy,omitempty' -l {} \; -exec perl -i -pe 's/h2_upgrade_policy,omitempty/h2UpgradePolicy,omitempty/g' {} \;
