db397f5676bce5c44d433b18b1d32cab44c28529
Sofie Gonzalez <sgonzalez@medium.com>
2024-09-25 12:35:43 -0700

    update picchu to go 1.22, keda  v2.13.0, prometheus-operator v0.71.0, controller-runtime v0.16.3

--- controllers/secretdeployer.go
+++ controllers/secretdeployer.go
@@ -7,7 +7,7 @@ import (
 	picchuv1alpha1 "go.medium.engineering/picchu/api/v1alpha1"
 
 	"github.com/go-logr/logr"
-	"github.com/prometheus/common/log"
+	"github.com/prometheus/common/promlog"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -32,17 +32,17 @@ func newSecretDeployer(client client.Client, log logr.Logger, secretList *corev1
 
 func (s *secretDeployer) deploy(ctx context.Context) error {
 	if len(s.secretList.Items) == 0 {
-		s.log.Info("No secrets found")
+		s.promlog.Info("No secrets found")
 	}
-	s.log.Info("Found secrets", "count", len(s.secretList.Items))
+	s.promlog.Info("Found secrets", "count", len(s.secretList.Items))
 	if err := s.syncNamespace(ctx); err != nil {
-		s.log.Info("Failed to sync namespace", "namespace.Name", s.instance.Spec.Target.Namespace)
+		s.promlog.Info("Failed to sync namespace", "namespace.Name", s.instance.Spec.Target.Namespace)
 		return err
 	}
 
 	errs := []error{}
 	for _, src := range s.secretList.Items {
-		s.log.Info("Syncing secret", "secret.Name", src.Name)
+		s.promlog.Info("Syncing secret", "secret.Name", src.Name)
 		dst := &corev1.Secret{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      src.Name,
@@ -58,7 +58,7 @@ func (s *secretDeployer) deploy(ctx context.Context) error {
 			return nil
 		})
 		if err != nil {
-			log.Error(err, "Failed to sync secret", "secret.Name", src.Name)
+			promlog.Error(err, "Failed to sync secret", "secret.Name", src.Name)
 			errs = append(errs, err)
 		}
 	}
@@ -78,6 +78,6 @@ func (s *secretDeployer) syncNamespace(ctx context.Context) error {
 	op, err := controllerutil.CreateOrUpdate(ctx, s.client, namespace, func() error {
 		return nil
 	})
-	s.log.Info("Namespace sync'd", "Op", op)
+	s.promlog.Info("Namespace sync'd", "Op", op)
 	return err
 }
