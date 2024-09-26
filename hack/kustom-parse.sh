9030e213992443124eb1b1a4c81e1ecb49c9adc4
Sofie Gonzalez <sgonzalez@medium.com>
2024-09-26 11:38:10 -0700

    remove cd

--- hack/kustom-parse.sh
+++ hack/kustom-parse.sh
@@ -6,7 +6,6 @@ which yq || python3 -m pip install yq
 
 pushd resources
 kustomize build ../config/crd | csplit - '/^---$/' {4} 
-cat config/crd/*
 for files in $(ls xx*)
     do new_path=$(cat $files | yq '.metadata.annotations."config.kubernetes.io/origin"'|cut -d: -f2|cut -d \\ -f1|xargs)
     echo "Copying $files --> $new_path"
