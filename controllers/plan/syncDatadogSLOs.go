273dafc2cfff6046bc986f48c8b868e9817c831c
Sofie Gonzalez <33813152+sofiegonzalez@users.noreply.github.com>
2025-01-30 08:17:12 -0800

    try to list ddog sos (#458)

--- controllers/plan/syncDatadogSLOs.go
+++ controllers/plan/syncDatadogSLOs.go
@@ -2,7 +2,9 @@ package plan
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
 
 	ddog "github.com/DataDog/datadog-operator/api/datadoghq/v1alpha1"
 	"github.com/go-logr/logr"
@@ -11,6 +13,8 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -26,6 +30,28 @@ type SyncDatadogSLOs struct {
 
 func (p *SyncDatadogSLOs) Apply(ctx context.Context, cli client.Client, cluster *picchuv1alpha1.Cluster, log logr.Logger) error {
 	datadogSLOs, err := p.datadogSLOs()
+	if p.App == "echo" {
+		// DD_API_KEY := os.Getenv("DD_API_KEY")
+		// log.Info("TEST ENV VAR ", "TEST ENV VAR  ", DD_API_KEY)
+
+		// DD_APPLICATION_KEY := os.Getenv("DD_API_KEY")
+		// log.Info("TEST ENV VAR ", "TEST ENV VAR  ", DD_APPLICATION_KEY)
+
+		// ctx := datadog.NewDefaultContext(context.Background())
+		configuration := datadog.NewConfiguration()
+		apiClient := datadog.NewAPIClient(configuration)
+		api := datadogV1.NewServiceLevelObjectivesApi(apiClient)
+		resp, r, err := api.ListSLOs(ctx, *datadogV1.NewListSLOsOptionalParameters())
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `ServiceLevelObjectivesApi.ListSLOs`: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		}
+
+		responseContent, _ := json.MarshalIndent(resp, "", "  ")
+		fmt.Fprintf(os.Stdout, "Response from `ServiceLevelObjectivesApi.ListSLOs`:\n%s\n", responseContent)
+
+	}
 	if err != nil {
 		return err
 	}
