d2c656e4e91ed5543be9998c1de33f3b85033ebd
Sofie Gonzalez <sgonzalez@medium.com>
2025-03-17 08:44:12 -0700

    implement datadog api so we can trigger canary when a datadogMonitor is triggered

--- controllers/revision_controller.go
+++ controllers/revision_controller.go
@@ -91,6 +91,16 @@ func (n *NoopPromAPI) IsRevisionTriggered(ctx context.Context, name, tag string,
 	return false, nil, nil
 }
 
+type DatadogAPI interface {
+	IsRevisionTriggered(ctx context.Context, name, tag string, withCanary bool) (bool, error)
+}
+
+type NoopDatadogAPI struct{}
+
+func (n *NoopDatadogAPI) IsRevisionTriggered(ctx context.Context, name, tag string, withCanary bool) (bool, error) {
+	return false, nil
+}
+
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler, c utils.Config) error {
 	_, err := builder.ControllerManagedBy(mgr).
@@ -118,6 +128,7 @@ type RevisionReconciler struct {
 	Scheme       *runtime.Scheme
 	Config       utils.Config
 	PromAPI      PromAPI
+	DatadogAPI   DatadogAPI
 	CustomLogger logr.Logger
 }
 
@@ -201,8 +212,18 @@ func (r *RevisionReconciler) Reconcile(ctx context.Context, request reconcile.Re
 	}
 
 	triggered, alarms, err := r.PromAPI.IsRevisionTriggered(context.TODO(), instance.Spec.App.Name, instance.Spec.App.Tag, instance.Spec.CanaryWithSLIRules)
+	ddog_triggered, ddog_err := r.DatadogAPI.IsRevisionTriggered(context.TODO(), instance.Spec.App.Name, instance.Spec.App.Tag, instance.Spec.CanaryWithSLIRules)
+
 	if err != nil {
 		return r.Requeue(log, err)
+	}
+
+	if ddog_err != nil {
+		log.Info("ignore datadog IsRevisionTriggered error for now")
+	}
+
+	if ddog_triggered {
+		log.Info("datadog IsRevisionTriggered returned true")
 	}
 
 	var revisionFailing bool
