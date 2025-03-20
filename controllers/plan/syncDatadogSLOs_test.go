7ceb70af833ece2afc4dd67ba5022c9c1f7f1225
Sofie Gonzalez <sgonzalez@medium.com>
2025-03-20 14:42:12 -0700

    add enabled toggle individual ddog slo

--- controllers/plan/syncDatadogSLOs_test.go
+++ controllers/plan/syncDatadogSLOs_test.go
@@ -34,6 +34,7 @@ var (
 		DatadogSLOs: []*picchuv1alpha1.DatadogSLO{
 			{
 				Name:        "istio-request-success",
+				Enabled:     true,
 				Description: "test create example datadogSLO one",
 				Query: picchuv1alpha1.DatadogSLOQuery{
 					GoodEvents:  "per_minute(sum:istio.mesh.request.count.total{(response_code:2* OR response_code:3* OR response_code:4*) AND destination_service:tutu.tutu-production.svc.cluster.local AND reporter:destination}.as_count())",
@@ -50,6 +51,7 @@ var (
 			},
 			{
 				Name:        "http-availability",
+				Enabled:     true,
 				Description: "test create example datadogSLO two",
 				Query: picchuv1alpha1.DatadogSLOQuery{
 					GoodEvents:  "per_minute(sum:istio.mesh.request.count.total{(response_code:2* OR response_code:3* OR response_code:4*) AND destination_service:echo.echo-production.svc.cluster.local AND reporter:destination}.as_count())",
