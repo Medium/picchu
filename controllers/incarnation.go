c0679bc76447256f8e89a7b3b805cd8564757c02
Sofie Gonzalez <33813152+sofiegonzalez@users.noreply.github.com>
2025-09-16 12:15:34 -0700

    open up datadog canary monitoring revert functionality to all services, do not query for production targets in events api, remove some logging (#564)

--- controllers/incarnation.go
+++ controllers/incarnation.go
@@ -18,7 +18,6 @@ import (
 	es "github.com/external-secrets/external-secrets/apis/externalsecrets/v1beta1"
 	"github.com/go-logr/logr"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/common/log"
 	istiov1alpha3 "istio.io/api/networking/v1alpha3"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -305,9 +304,6 @@ func (i *Incarnation) isCanaryPending() bool {
 	if target == nil {
 		return false
 	}
-	if i.appName() == "echo" {
-		log.Info("isCanaryPending() incarnation - Checking if Datadog canary is pending", "target", target.Name, "canary start timestamp", i.status.CanaryStartTimestamp, "iscanarypending", target.IsCanaryPending(i.status.CanaryStartTimestamp))
-	}
 	return target.IsCanaryPending(i.status.CanaryStartTimestamp)
 }
 
@@ -315,9 +311,6 @@ func (i *Incarnation) datadogMonitoring() bool {
 	target := i.target()
 	if target == nil {
 		return false
-	}
-	if i.appName() == "echo" {
-		log.Info("datadogMonitoring() - Checking if Datadog monitoring is enabled for incarnation", "target", target.Name, "ddog monitoring", target.DatadogMonitoring.Enabled)
 	}
 	return target.DatadogMonitoring.Enabled
 }
