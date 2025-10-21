72d97a3e31ef83a00f66e0121586f09ed777f5e7
Sofie Gonzalez <33813152+sofiegonzalez@users.noreply.github.com>
2025-10-21 08:21:51 -0700

    update datadog canary alert message (#568)
    * improve canary alerting
    
    * update alert message
    
    * fix monitor id string

--- slack/slack_api.go
+++ slack/slack_api.go
@@ -2,8 +2,11 @@ package slack
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
 	slack "github.com/slack-go/slack"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
@@ -37,11 +40,38 @@ func InjectSlackAPI(a SlackAPI) *SLACKAPI {
 }
 
 // test channel #eng-fredbottest: C02EKA9SB
-func (a SLACKAPI) PostMessage(ctx context.Context, app string, tag string) (bool, error) {
+func (a SLACKAPI) PostMessage(ctx context.Context, app string, tag string, eventAttributes *datadogV2.EventAttributes) (bool, error) {
+	monitor_name := *eventAttributes.Monitor.Get().Name
+	monitor_id := *eventAttributes.Monitor.Get().Id
+
+	iris_url := fmt.Sprintf("https://iris.medium.build/servers/%s/revisions/%s?target=production", app, tag)
+	dataog_url := "https://app.datadoghq.com/monitors/" + fmt.Sprint(monitor_id) + "?q=version%3A" + tag
+
+	elements := make([]slack.BlockElement, 0, 3)
+	addLink := func(label, link string) {
+		textBlock := slack.NewTextBlockObject("plain_text", label, true, false)
+		button := slack.NewButtonBlockElement("", label, textBlock)
+		button.URL = link
+		elements = append(elements, button)
+	}
+	addLink(":iris: Iris", iris_url)
+	addLink(":datadog: Datadog Triggered Monitor", dataog_url)
+	addLink(":notion: Canary Runbook", "")
+
+	block := slack.NewActionBlock("useful_links", elements...)
+
+	text := ":failure-error-cross-x: :canary: *" + strings.ToUpper("echo") + "* release `" + "main-20251017-181807-e7e818488e" + "` is failing Datadog Canary\n:datadog: Triggered Monitor `" + monitor_name + "`\n"
+	textBlock := slack.NewTextBlockObject("mrkdwn", text, false, false)
+
+	opts := []slack.MsgOption{
+		slack.MsgOptionText(text, false),
+		slack.MsgOptionAsUser(true),
+		slack.MsgOptionBlocks(slack.NewSectionBlock(textBlock, nil, nil), block),
+	}
+
 	respChannelID, timestamp, err := a.api.PostMessage(
 		"C02EKA9SB",
-		slack.MsgOptionText("Canary is Failing for "+app+" - Picchu detected an issue with revision "+tag, false),
-		slack.MsgOptionAsUser(true), // Add this if you want that the bot would post message as a user, otherwise it will send response using the default slackbot
+		opts...,
 	)
 	if err != nil {
 		slack_log.Error(err, "Error when calling `PostMessage`\n", "error", err, "respChannelID", respChannelID)
